5e60fb024f977113e5d93dc177d9bf4ba27bdd58
Naila Putri Zanetha <nailazanethaa@gmail.com>
2026-03-02 22:10:53 +0700

    Add SLURM job submission script for LAMMPS

---
+++ lmpui/submit/submit.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=JOBNAME
+#SBATCH --output=output/JOBNAME.out
+#SBATCH --error=output/JOBNAME.err
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --partition=guriang
+
+export OMP_PROC_BIND=true
+export NUMBA_NUM_THREADS=$SLURM_NPROCS
+export OMP_NUM_THREADS=$SLURM_NPROCS
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+
+echo "$PWD"
+mpirun /home/share/bin/lmp_mpi -in INPUT_FILE
