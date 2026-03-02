8a27f826a58117c285924941a9b3a7b0a5a66270
Naila Putri Zanetha <nailazanethaa@gmail.com>
2026-03-02 21:07:15 +0700

    Fix indentation for creating Python virtual environment

---
+++ entry-head.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ ! -z $LMPUSER ]]; then
+        (id $LMPUSER>/dev/null 2>&1) || useradd $LMPUSER -m -s /bin/bash
+        (grep -q $LMPUSER /etc/sudoers) || (echo "$LMPUSER ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers)
+        echo '[[ -z $SUDO_USER ]] && exec runuser -l $LMPUSER' > /root/.bashrc
+        runuser -u $LMPUSER -- ssh-keygen -t ed25519 -N "" -f /home/$LMPUSER/.ssh/id_ed25519 -q
+        runuser -u $LMPUSER -- cp /home/$LMPUSER/.ssh/id_ed25519.pub /home/$LMPUSER/.ssh/authorized_keys
+        runuser -u $LMPUSER -- mkdir /home/$LMPUSER/venv
+        runuser -u $LMPUSER -- python3 -m venv /home/$LMPUSER/venv
+fi
+
+echo Booting head node: ghead
+service dbus start
+service ssh start
+service munge start
+
+mkdir -p /home/share/bin
+mkdir -p /home/share/files
+if [[ ! -x /home/share/bin/lmp_mpi ]]; then
+        cp /opt/bin/lmp_mpi /home/share/bin/
+        cp -a /opt/potentials /home/share/files/
+fi
+
+if [[ -z $NOSLURM ]]; then
+        slurmctld -D
+else
+        while true; do
+                sleep 10
+        done
+fi
