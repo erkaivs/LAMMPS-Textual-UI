29c9db7805cbe0773cfca9424bd8316db878d53c
Nero Zane <nailazanethaa@gmail.com>
2026-04-11 18:29:44 +0700

    Update submit.bash

--- lmpui/lmpui_template/submit/submit.bash
+++ lmpui/lmpui_template/submit/submit.bash
@@ -11,7 +11,5 @@ export NUMBA_NUM_THREADS=$SLURM_NPROCS
 export OMP_NUM_THREADS=$SLURM_NPROCS
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 
-cd "$SLURM_SUBMIT_DIR"
-
 echo "$PWD"
 mpirun /home/share/bin/lmp_mpi -in INPUT_FILE
